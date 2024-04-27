7ee59ea3f15cca4523afe80e11533ee27065adda
dailingx <468551414@qq.com>
2024-04-27 18:23:53 +0800

    add test demo

--- vidfetch/script/push_file_to_hf_demo.py
+++ vidfetch/script/push_file_to_hf_demo.py
@@ -1,7 +1,7 @@
 from vidfetch.api.huggingface import push_file_to_hf
 
 if __name__ == '__main__':
-    hf_token = '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'
+    hf_token = ''
     hf_repo_id = 'OpenVideo/YouTube-Commons-5G-Raw'
     file_path = '/mnt/application/test.mp4'
     path_in_repo = 'test.mp4'
